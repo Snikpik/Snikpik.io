bf4dfb9f9f88fabc0652202432f1270b47092cfd
Valentin Prugnaud <valentin@whatdafox.com>
2016-06-13 20:57:22 -0700

    Facebook Open Graph update

--- resources/views/vendor/spark/layouts/app.blade.php
+++ resources/views/vendor/spark/layouts/app.blade.php
@@ -5,9 +5,22 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="token" content="{{ csrf_token() }}">
+    <meta name="description" content="Display website previews on your website using our API.">
+    <meta name="robots" content="index, nofollow">
+    <meta name="author" content="Valentin Prugnaud">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>@yield('title', 'Snikpik :: Website preview made simple.')</title>
+
+
+    <!-- Facebook -->
+    <meta property="og:title" content="Website preview made easy">
+    <meta property="og:site_name" content="Snikpik.io">
+    <meta property="og:url" content="https://snikpik.io">
+    <meta property="og:description" content="Display website previews on your website using our API.">
+    <meta property="fb:app_id" content="841769272634645">
+    <meta property="og:type" content="website">
+    <meta property="og:image" content="{{ asset('img/color-logo.png') }}">
 
     <!-- CSS -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -24,6 +37,23 @@
     </script>
 </head>
 <body class="with-navbar" v-cloak>
+    <script>
+        window.fbAsyncInit = function() {
+            FB.init({
+                appId      : '841769272634645',
+                xfbml      : true,
+                version    : 'v2.6'
+            });
+        };
+
+        (function(d, s, id){
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) {return;}
+            js = d.createElement(s); js.id = id;
+            js.src = "//connect.facebook.net/en_US/sdk.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+    </script>
     <div id="spark-app">
         <!-- Navigation -->
         @if (Auth::check())
